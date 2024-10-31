abff6dd1cc688bb535c91fdaa2eea957da4ddb74
ElectroByteSystems <luishalarcon572@gmail.com>
2024-10-31 08:39:18 -0600

    coming

---
+++ Numeros Bernoulli/Numeros Bernoulli/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Numeros_Bernoulli
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Titulo = new System.Windows.Forms.Label();
+            this.Instrucciones = new System.Windows.Forms.Label();
+            this.txtIndice = new System.Windows.Forms.TextBox();
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.lblRespuesta = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Titulo
+            // 
+            this.Titulo.AutoSize = true;
+            this.Titulo.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Titulo.Location = new System.Drawing.Point(406, 31);
+            this.Titulo.Name = "Titulo";
+            this.Titulo.Size = new System.Drawing.Size(314, 46);
+            this.Titulo.TabIndex = 0;
+            this.Titulo.Text = "Numeros Bernolli";
+            // 
+            // Instrucciones
+            // 
+            this.Instrucciones.AutoSize = true;
+            this.Instrucciones.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Instrucciones.Location = new System.Drawing.Point(12, 147);
+            this.Instrucciones.Name = "Instrucciones";
+            this.Instrucciones.Size = new System.Drawing.Size(645, 46);
+            this.Instrucciones.TabIndex = 1;
+            this.Instrucciones.Text = "Ingrese el índice del número Bernolli:";
+            // 
+            // txtIndice
+            // 
+            this.txtIndice.Location = new System.Drawing.Point(692, 147);
+            this.txtIndice.Name = "txtIndice";
+            this.txtIndice.Size = new System.Drawing.Size(100, 44);
+            this.txtIndice.TabIndex = 2;
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.Location = new System.Drawing.Point(817, 134);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(219, 57);
+            this.btnConvertir.TabIndex = 3;
+            this.btnConvertir.Text = "Convertir";
+            this.btnConvertir.UseVisualStyleBackColor = true;
+            this.btnConvertir.Click += new System.EventHandler(this.btnConvertir_Click);
+            // 
+            // lblRespuesta
+            // 
+            this.lblRespuesta.AutoSize = true;
+            this.lblRespuesta.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRespuesta.Location = new System.Drawing.Point(419, 271);
+            this.lblRespuesta.Name = "lblRespuesta";
+            this.lblRespuesta.Size = new System.Drawing.Size(256, 61);
+            this.lblRespuesta.TabIndex = 4;
+            this.lblRespuesta.Text = "Respuesta";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(1071, 411);
+            this.Controls.Add(this.lblRespuesta);
+            this.Controls.Add(this.btnConvertir);
+            this.Controls.Add(this.txtIndice);
+            this.Controls.Add(this.Instrucciones);
+            this.Controls.Add(this.Titulo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Titulo;
+        private System.Windows.Forms.Label Instrucciones;
+        private System.Windows.Forms.TextBox txtIndice;
+        private System.Windows.Forms.Button btnConvertir;
+        private System.Windows.Forms.Label lblRespuesta;
+    }
+}
+
---
+++ Numeros Bernoulli/Numeros Bernoulli/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Numeros_Bernoulli
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvertir_Click(object sender, EventArgs e)
+        {
+            int Indice;
+            if (int.TryParse(txtIndice.Text, out Indice) && Indice >= 0 && Indice <= 100) 
+            {
+                double resultado = CalcularBernoulli(Indice);
+                lblRespuesta.Text = $"B{Indice} = {resultado}";
+            }
+            else 
+            {
+                MessageBox.Show("Por favor, ingrese un número entero entre 0 y 100.");
+            }
+        }
+        private double CalcularBernoulli(int n)
+        {
+            // Si n es impar y mayor a 1, el número de Bernoulli es 0
+            if (n > 1 && n % 2 != 0)
+            {
+                return 0;
+            }
+
+            // Array para almacenar los valores intermedios de Bernoulli
+            double[] A = new double[n + 1];
+
+            // Implementación de la fórmula de Akiyama-Tanigawa
+            for (int m = 0; m <= n; m++)
+            {
+                A[m] = 1.0 / (m + 1);
+                for (int j = m; j >= 1; j--)
+                {
+                    A[j - 1] = j * (A[j - 1] - A[j]);
+                }
+            }
+
+            // Retornar el valor de Bn
+            return n == 1 ? -0.5 : A[0]; // B1 es -1/2, el resto según el cálculo.
+        }
+    }
+}
